f173655f0dd020f338e898b84456f28687a779db
Lucas Oliveira <lucasoliveira226@gmail.com>
2022-03-27 22:38:01 -0300

    Refatorando para extrair mais trechos de código

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const crypto = require('crypto');
 const {
   validarEmail,
   validarSenha,
-} = require('./middlewares/index.js')
+  validarNome,
+  validarIdade,
+} = require('./middlewares/index.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,38 +20,6 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
-// Por algum motivo, a importação das validações de email e senha não estão funcionando, portanto inseri no arquivo principal
-// const validarEmail = (request, response, next) => {
-//   const { email } = request.body;
-//   const mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-
-//   if (!email || email === '') {
-//     return response.status(400).json({ message: 'O campo "email" é obrigatório' });
-//   }
-
-//   if (email.match(mailformat)) {
-//     next();
-//   } else {
-//     return response.status(400)
-//     .json({ message: 'O "email" deve ter o formato "email@email.com"' });
-//   }
-//   next();
-// };
-
-// const validarSenha = (request, response, next) => {
-//   const { password } = request.body;
-
-//   if (!password) {
-//     return response.status(400).json({ message: 'O campo "password" é obrigatório' });
-//   }
-
-//   if (password.length < 6) {
-//     return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-//   }
-
-//   next();
-// };
 
 const validarToken = (request, response, next) => {
   const { authorization } = request.headers;
@@ -87,35 +57,8 @@ app.post('/login', validarEmail, validarSenha, (request, response) => {
   response.status(200).json({ token });
 });
 
-function validarNome(request, response, next) {
-  const { name } = request.body;
-
-  if (!name) {
-    return response.status(400).json({ message: 'O campo "name" é obigatório' });
-  }
-
-  if (name.length < 3) {
-    return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  }
-
-  next();
-}
-
-function validarIdade(request, response, next) {
-  const { age } = request.body;
-  if (!age) {
-    return response.status(400).json({ message: 'O campo "age" é obrigatório' });
-  }
-
-  if (age < 18) {
-    return response.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
-  }
-
-  next();
-}
-
-app.post('/talker', validarToken, validarNome, validarIdade, (request, response) => {
-
+app.post('/talker', validarToken, validarNome, validarIdade, (_request, _response) => {
+  console.log('teste');
 });
 
 app.listen(PORT, () => {
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,11 @@
 const validarEmail = require('./validarEmail');
 const validarSenha = require('./validarSenha');
+const validarNome = require('./validarNome');
+const validarIdade = require('./validarIdade');
 
 module.exports = {
   validarEmail,
   validarSenha,
+  validarNome,
+  validarIdade,
 };
